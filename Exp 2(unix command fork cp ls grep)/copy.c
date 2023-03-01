56641ff99d11163f45fb99edd41cd40e06d71abd
Amil P A <117823429+amilpa@users.noreply.github.com>
2023-03-01 14:38:56 +0530

    unix command simulation

---
+++ Exp 2(unix command fork cp ls grep)/copy.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+int main(int argc , char *argv[])
+{
+	FILE *fp1 , *fp2;
+	char ch;
+	int sc = 0;
+
+	fp1 = fopen(argv[1],"r");
+	fp2 = fopen(argv[2],"w");
+
+	if(fp1==NULL)
+	{
+		printf("Unable to open the given file \n");
+	}
+	else
+	{
+		while((ch = fgetc(fp1))!=EOF)
+		{
+			if(ch == ' ')
+			{
+				sc++;
+			}
+			fputc(ch , fp2);
+		}
+		printf("no of spaces %d",sc);
+		printf("\n");
+		fclose(fp1);
+		fclose(fp2);
+	}
+}
